b167cd4ab41d3387b209caeae263cce516ede6a6
lthsieh <two.frank@gmail.com>
2020-10-04 21:31:15 -0700

    segmented latitude and longitude into bins and generated 15-min time bins

--- covid_alliance_person_level_risk_score.sql
+++ covid_alliance_person_level_risk_score.sql
@@ -15,4 +15,18 @@ with tab1 as (
 from "ANALYTICS"."DEV"."AZ_CLUSTERS_7_15_to_7_22"
 where dwell_time_minutes > 5
 order by lat_bins, long_bins
-),  -- tab1: generate lat_bins and long_bins 
+),  -- tab1: generate lat_bins and long_bins
+
+tab2 as (
+  select DISTINCT(CLUSTER_START_SLICE_S) as interval_start,
+         DATEADD('minute', 15, interval_start) as interval_end
+  from
+  (select CLUSTER_STARTED_UTC,
+          time_slice(CLUSTER_STARTED_UTC, 15, 'minutes', 'START') as CLUSTER_START_SLICE_S,
+          time_slice(CLUSTER_STARTED_UTC, 15, 'minutes', 'END')   as CLUSTER_START_SLICE_E,
+          DWELL_TIME_MINUTES
+   from "ANALYTICS"."DEV"."AZ_CLUSTERS_7_15_to_7_22"
+   --where day(CLUSTER_STARTED_UTC) = '17'
+   order by CLUSTER_STARTED_UTC)
+   order by 1
+),  -- tab2: generate 15-min time intervals
